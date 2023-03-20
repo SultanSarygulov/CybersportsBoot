0924534b44dc32aca6942da01eb9a7d19f8062d6
SultanSarygulov <itsme.kaan.sarygulov@gmail.com>
2023-03-20 18:13:10 +0600

    Replaced to project created with spring

---
+++ src/test/java/com/noliferpedia/noliferpedia/NoliferpediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noliferpedia.noliferpedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoliferpediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
